b7868b8b990e405355fba7ae05f53f0ca60b56a3
iLynette <lynetteacholah@icloud.com>
2022-10-03 18:11:37 +0300

    Generate rooms controller

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,59 @@
+class RoomsController < ApplicationController
+  def index
+    @rooms = Room.all
+  end
+
+  def show
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def edit
+  end
+
+  def create
+    @room = Room.new(room_params)
+
+    respond_to do |format|
+      if @room.save
+        format.html { redirect_to room_url(@room), notice: "Room was successfully created." }
+        format.json { render :show, status: :created, location: @room }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @room.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @room.update(room_params)
+        format.html { redirect_to room_url(@room), notice: "Room was successfully updated." }
+        format.json { render :show, status: :ok, location: @room }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @room.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @room.destroy
+
+    respond_to do |format|
+      format.html { redirect_to rooms_url, notice: "Room was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_room
+      @room = Room.find(params[:id])
+    end
+
+    def room_params
+      params.require(:room).permit(:name)
+    end
+end
