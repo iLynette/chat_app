3401a80f179f88ccce4a0792636e02e4c983049e
iLynette <lynetteacholah@icloud.com>
2022-10-04 17:01:44 +0300

    Add messages in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :messages
   devise_for :users
   resources :rooms
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
