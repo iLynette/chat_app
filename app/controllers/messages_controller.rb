94dcab6980eea2c9a8813e59db9a44f4eb20b0c6
iLynette <lynetteacholah@icloud.com>
2022-10-04 16:58:10 +0300

    Generate messages controller

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,61 @@
+class MessagesController < ApplicationController
+  before_action :set_message, only: %i[ show edit update destroy ]
+
+  def index
+    @messages = Message.all
+  end
+
+  def show
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def edit
+  end
+
+  def create
+    @message = Message.new(message_params)
+
+    respond_to do |format|
+      if @message.save
+        format.html { redirect_to message_url(@message), notice: "Message was successfully created." }
+        format.json { render :show, status: :created, location: @message }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @message.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @message.update(message_params)
+        format.html { redirect_to message_url(@message), notice: "Message was successfully updated." }
+        format.json { render :show, status: :ok, location: @message }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @message.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @message.destroy
+
+    respond_to do |format|
+      format.html { redirect_to messages_url, notice: "Message was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_message
+      @message = Message.find(params[:id])
+    end
+
+    def message_params
+      params.require(:message).permit(:content, :room_id, :user_id)
+    end
+end
