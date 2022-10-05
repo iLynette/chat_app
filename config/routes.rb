aae1a4491f034760704282c7e48b9e49f63770d7
iLynette <lynetteacholah@icloud.com>
2022-10-05 17:52:37 +0300

    Nest messages in rooms

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :messages
   devise_for :users
-  resources :rooms
+  resources :rooms do
+    resources :messages
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
