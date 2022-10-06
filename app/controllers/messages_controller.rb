aaa96a5727cf1efb48fdae6c7b3207abebd8879f
iLynette <lynetteacholah@icloud.com>
2022-10-06 14:29:04 +0300

    Redirect messages to show page for rooms

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -21,7 +21,7 @@ class MessagesController < ApplicationController
 
     respond_to do |format|
       if @message.save
-        format.html { redirect_to message_url(@message), notice: "Message was successfully created." }
+        format.html { redirect_to @message.room, notice: "Message was successfully created." }
         format.json { render :show, status: :created, location: @message }
       else
         format.html { render :new, status: :unprocessable_entity }
